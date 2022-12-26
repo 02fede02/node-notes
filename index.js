837ecb16184182191682a385ccfeaa00f5c08640
02fede02 <fede.birman93@gmail.com>
2022-12-26 12:26:55 -0300

    Testing changes

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get('/', (request, response) => {
 
 app.get('/notes', (request, response) => {
 	const note = request.body
+	console.log(note)
 	if(!note || !note.content) {
 		return response.status(400).json({
 			error: 'note.content is missingasdfa'
